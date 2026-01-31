c90bc3b4378af86429344046c81ba55412c3ed4d
Vincent Långström <vincent.langstrom@gmail.com>
2026-01-31 14:37:45 +0200

    Fix flaky arg parsing?

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 set -e
 
+# Quote each argument for the inner shell: wrap in single quotes, escape ' as '\''
+# so the -c string passes args through without space/split or quoting issues
+quoted=""
+for a in "$@"; do
+  safe=$(echo "$a" | sed "s/'/'\\\\''/g")
+  quoted="$quoted '$safe'"
+done
+
 # Execute cyanrip inside a PTY and normalize CR to LF
-exec script -q -c "/usr/local/bin/cyanrip $*" /dev/null | tr '\r' '\n'
+exec script -q -c "/usr/local/bin/cyanrip $quoted" /dev/null | tr '\r' '\n'
